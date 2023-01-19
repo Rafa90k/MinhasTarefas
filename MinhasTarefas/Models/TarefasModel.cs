1e16c455b7cad1a5a8b603e73e88e6738ef72d4e
rafael <embau12rafael@gmail.com>
2023-01-18 23:05:40 -0300

    feat(MinhasTarefas): Adicionando os models

---
+++ MinhasTarefas/Models/TarefasModel.cs
@@ -0,0 +1,12 @@
+namespace MinhasTarefas.Models;
+
+public class TarefasModel
+{
+    public int Id { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? Descricao { get; set; }
+    
+    public int Status { get; set; }
+}
---
+++ MinhasTarefas/Models/UsuariosModel.cs
@@ -0,0 +1,11 @@
+namespace MinhasTarefas.Models;
+
+public class UsuariosModel
+
+{
+    public int Id { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? Email { get; set; }
+}
